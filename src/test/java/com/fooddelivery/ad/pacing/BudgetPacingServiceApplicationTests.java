272a1f15633fae69e8749d069447fac8c878eefc
Parthu <parthudhanireddy@gmail.com>
2026-08-25 06:31:39 +0530

    chore: commit local changes for Platform Hardening

--- src/test/java/com/fooddelivery/ad/pacing/BudgetPacingServiceApplicationTests.java
+++ src/test/java/com/fooddelivery/ad/pacing/BudgetPacingServiceApplicationTests.java
@@ -2,6 +2,9 @@ package com.fooddelivery.ad.pacing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.fooddelivery.advertisement.pacing.BudgetPacingApplication;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,5 +21,13 @@ class BudgetPacingServiceApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    void outboxProcessorBeanExists() {
+        assertTrue(applicationContext.containsBean("outboxProcessor"), "OutboxProcessor bean should be present");
     }
 }
