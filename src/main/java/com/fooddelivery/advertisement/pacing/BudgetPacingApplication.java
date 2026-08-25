9df95161fbfdb260fb2b52d89fac105b07bed4c0
Parthu <parthudhanireddy@gmail.com>
2026-08-25 18:01:54 +0530

    Fix Application configuration scanning and add spring-cloud-starter-config

--- src/main/java/com/fooddelivery/advertisement/pacing/BudgetPacingApplication.java
+++ src/main/java/com/fooddelivery/advertisement/pacing/BudgetPacingApplication.java
@@ -15,8 +15,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 )
 @EnableScheduling
 @EnableFeignClients
-@org.springframework.boot.autoconfigure.domain.EntityScan(basePackages = {"com.fooddelivery.advertisement.pacing", "com.fooddelivery.common.entity"})
-@org.springframework.data.jpa.repository.config.EnableJpaRepositories(basePackages = {"com.fooddelivery.advertisement.pacing", "com.fooddelivery.common.repository"})
+@org.springframework.boot.autoconfigure.domain.EntityScan(basePackages = {"com.fooddelivery.advertisement.pacing", "com.fooddelivery.common"})
+@org.springframework.data.jpa.repository.config.EnableJpaRepositories(basePackages = {"com.fooddelivery.advertisement.pacing", "com.fooddelivery.common"})
 @Import(NotificationRouterService.class)
 public class BudgetPacingApplication {
     public static void main(String[] args) {
